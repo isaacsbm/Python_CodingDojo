30225ebb09426004f320dc7ad9b95f9cc3e0926b
isaacsbm <isaacsonbm@gmail.com>
2023-04-05 16:33:50 -0700

    main

--- testing.py
+++ testing.py
@@ -1,12 +1,5 @@
-print ('Hello World')
-
-import random
-roll = random.randint(1,20)
-print(roll)
-
-num_array = []
-for i in range(20):
-    num_array.append(random.randint(1,100))
-
-print(num_array)
-print(num_array[random.randint(0,len(num_array))]) 
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+# output: s, t, r
